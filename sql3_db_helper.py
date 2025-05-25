7a334c41b9052740182f311b19ff2f708c966c14
Amoosekerrason <138499289+Amoosekerrason@users.noreply.github.com>
2025-05-25 20:07:36 +0800

    initial SQ3DB stuff

--- sql3_db_helper.py
+++ sql3_db_helper.py
@@ -3,6 +3,23 @@ from abstract_class import DBHelper, DBQueue
 import os
 
 
+class SQL3DBqueue(DBQueue):
+    def create(self, table_name, columns):
+        return super().create(table_name, columns)
+
+    def insert(self, table, columns, values):
+        return super().insert(table, columns, values)
+
+    def update(self, table, set_values, where=None):
+        return super().update(table, set_values, where)
+
+    def delete(self, table, where=None):
+        return super().delete(table, where)
+
+    def select(self, table, columns, where=None):
+        return super().select(table, columns, where)
+
+
 class SQL3DBHelper(DBHelper):
     def __init__(self, db_path):
         super().__init__(db_path)
